499bd31f0a75d4f742933a876aba6e3c5fc92a58
GitHub Action <action@github.com>
2025-01-14 16:26:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/ondrejspanel/intellijprivatepluginauth/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ondrejspanel/intellijprivatepluginauth/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ondrejspanel.intellijprivatepluginauth.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ondrejspanel.intellijprivatepluginauth.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
